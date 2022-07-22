cb310d18a01d12ca21ac03b83f3be1aeeae967e2
Adam Aguirre <adamaguirre@weber.edu>
2022-07-22 16:42:54 -0600

    Base Key pad

---
+++ PlazmaELGGOB/Moddel/BoggleKey.cs
@@ -0,0 +1,20 @@
+﻿namespace PlazmaELGGOB.Moddel
+{
+    public class BoggleKey
+    {
+        public int Space { get; set; }
+        public char Letter { get; set; }
+        public bool Clicked { get; set; }
+
+        // this will include which indexes are alowed to be clicked, folowing this button being clicked.
+        public int[] allowedIndexes { get; set; }
+
+        public BoggleKey(int space, char letter, bool clicked, int[] allowedIndexes)
+        {
+            Space = space;
+            Letter = letter;
+            Clicked = clicked;
+            this.allowedIndexes = allowedIndexes;
+        }
+    }
+}
