102a1cfb1763a862c3114be9637c6883e3cf94cf
Rbackwards <49381445+rbackwards@users.noreply.github.com>
2022-08-01 19:19:18 -0600

    added user connection count.

--- PlazmaELGGOB/Hubs/PointHub.cs
+++ PlazmaELGGOB/Hubs/PointHub.cs
@@ -5,6 +5,16 @@ using System.Timers;
 
 namespace PlazmaELGGOB.Hubs
 {
+
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+
+        public static int getConnectedIdCount()
+        {
+            return ConnectedIds.Count;
+        }
+    }
     public class PointHub : Hub
     {
         private System.Timers.Timer aTimer;
@@ -12,6 +22,22 @@ namespace PlazmaELGGOB.Hubs
         public Task SendMessage(string user, string points)
         {
             return Clients.All.SendAsync("RecieveMessage", user, points);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+
+            Console.WriteLine("users connected " + UserHandler.ConnectedIds.Count);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+
+            Console.WriteLine("users disconnected " + UserHandler.ConnectedIds.Count);
+            return base.OnDisconnectedAsync(exception);
         }
         //public Task UpdateTimer(int timer)
         //{        
