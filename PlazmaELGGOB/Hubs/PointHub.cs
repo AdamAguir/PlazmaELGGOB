217399667417037c662b5f8a9b7e5eeb9306027f
Rbackwards <49381445+rbackwards@users.noreply.github.com>
2022-08-08 18:31:35 -0600

    timer is synced up homies

--- PlazmaELGGOB/Hubs/PointHub.cs
+++ PlazmaELGGOB/Hubs/PointHub.cs
@@ -17,8 +17,7 @@ namespace PlazmaELGGOB.Hubs
     }
     public class PointHub : Hub
     {
-        private System.Timers.Timer aTimer;
-        private int timeRemaning;
+        
         public Task SendMessage(string user, string points)
         {
             return Clients.All.SendAsync("RecieveMessage", user, points);
@@ -39,24 +38,11 @@ namespace PlazmaELGGOB.Hubs
             Console.WriteLine("users disconnected " + UserHandler.ConnectedIds.Count);
             return base.OnDisconnectedAsync(exception);
         }
-        //public Task UpdateTimer(int timer)
-        //{        
-        //    return Clients.All.SendAsync("UpdateTimer", timer);
-        //}
-
-        //public void startTimer()
-        //{
-        //    timeRemaning = 60;
-        //    //create timer at 1 second interval
-        //    aTimer = new System.Timers.Timer(1000);
-        //    aTimer.Elapsed += OnTimedEvent;
-        //    aTimer.AutoReset = true;
-        //    aTimer.Enabled = true;
-        //}
-        //private void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
-        //    timeRemaning -= 1;
-        //}
+        
+        public Task TimerCheck()
+        {
+            return Clients.All.SendAsync("StartTimer");
+        }
 
     }
 }
